0737b2d3f390d82c225743b0cf3037780080fa0b
Addictive <163709843+Subin-Lim@users.noreply.github.com>
2025-03-27 19:25:11 +0900

    Update mars_mission_computer.py

--- mars_mission_computer.py
+++ mars_mission_computer.py
@@ -1,16 +1,16 @@
 import random
-import time
 
 class DummySensor:
     def __init__(self):
         self.env_values = {
-            'mars_base_internal_temperature': 0.0,
-            'mars_base_external_temperature': 0.0,
-            'mars_base_internal_humidity': 0.0,
-            'mars_base_external_illuminance': 0.0,
-            'mars_base_internal_co2': 0.0,
-            'mars_base_internal_oxygen': 0.0
+            'mars_base_internal_temperature': 20.0,
+            'mars_base_external_temperature': 10.0,
+            'mars_base_internal_humidity': 55.0,
+            'mars_base_external_illuminance': 600.0,
+            'mars_base_internal_co2': 0.05,
+            'mars_base_internal_oxygen': 5.0
         }
+    
     def set_env(self):
         self.env_values['mars_base_internal_temperature'] = round(random.uniform(18.0, 30.0), 2)
         self.env_values['mars_base_external_temperature'] = round(random.uniform(0.0, 21.0), 2)
@@ -20,7 +20,7 @@ class DummySensor:
         self.env_values['mars_base_internal_oxygen'] = round(random.uniform(4.0, 7.0), 2)
 
     def get_env(self):
-        current_time = time.strftime('%Y-%m-%d %H:%M:%S') 
+        current_time = "2025-03-27 08:30:00"  
         log_data = (
             f'{current_time} \n'
             f"화성 기지 내부 온도: {self.env_values['mars_base_internal_temperature']} 도\n"
@@ -36,11 +36,11 @@ class DummySensor:
             file.write(log_data)
 
         return self.env_values
-    
+
 if __name__ == '__main__':
-    ds = DummySensor()      
-    ds.set_env()            
-    env_data = ds.get_env() 
+    ds = DummySensor()
+    ds.set_env()  
+    env_data = ds.get_env()  
 
     print('[더미 센서 값 출력]')
     for key, value in env_data.items():
